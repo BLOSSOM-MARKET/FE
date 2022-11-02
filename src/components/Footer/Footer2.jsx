acacf876ad9d7150fd01cec4119a382c29bddfcb
윤혜구/시스템개발팀/223596 <223596@ssg.global>
2022-11-02 17:31:27 +0900

    item new style 변경

--- src/components/Footer/Footer2.jsx
+++ src/components/Footer/Footer2.jsx
@@ -9,7 +9,16 @@ const Footer = () => {
                <div className={style.Footer__inner__text}>
                     <p>사내 중고거래 플랫폼 <strong>블라썸 마켓</strong></p>
                     <p>신세계I&C 2022 신입 프로젝트 C조</p>
-                    <p>윤혜구 (조장/시스템개발팀), 조성현(스타벅스운영1팀), 전중훈(스타벅스운영1팀), 최정윤(스타벅스개발팀), 이지혁(데이터센터팀), 함지용(데이터센터팀), 황신지(백화점1팀)</p>
+                    <p><strong>윤혜구</strong> (조장/시스템개발팀), <strong>조성현</strong> (스타벅스운영1팀), <strong>전중훈</strong> (스타벅스운영1팀), <strong>최정윤</strong> (스타벅스개발팀), <strong>이지혁</strong> (데이터센터팀), <strong>함지용</strong> (데이터센터팀), <strong>황신지</strong> (백화점1팀)</p>
+
+                    <div className={style.Footer__inner__inc}>
+                        <img src="/inc_logo.png" alt="ssg_logo" className={style.Footer__inner__inc__logo} />
+                        <span>서울특별시 중구 남대문시장10길 2, 메사빌딩 21층</span>
+                        <span><strong>대표이사</strong> 형태준</span>
+                        <span><strong>TEL</strong> 02-3397-1234</span>
+                        <span><strong>FAX</strong> 02-3397-1099</span>
+                    </div>
+                    <div className={style.Footer__inner__bottom}>Copyright ⓒ 2020 SHINSEGAE I&C Inc. All Rights Reserved.</div>
                </div>
             </div>
         </div>
--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -21,10 +21,12 @@ import styleLabel from "../components/Upload/Upload.module.scss";
 </script> */}
 
 const Uploadpage = (props) => {
+    const [titleLength, setTitleLength] = useState(0);
+    const [contentLength, setContentLength] = useState(0);
 
     //이미지 경로 상태관리 추가
     const [imageUrl, setImageUrl] = useState(null);
-
+    
     //이미지 처리 함수
     const onChangeImage = (info) => {
         //파일이 업로드 중일 때
@@ -43,8 +45,10 @@ const Uploadpage = (props) => {
         }
     }
 
-    function lengthCheck(str) {
-        return str.value.length
+    function lengthCheck(e) {
+        console.log(e)
+        const len = e.target.value.length;
+        setTitleLength(len);
     }
 
     return (
@@ -53,58 +57,73 @@ const Uploadpage = (props) => {
                 <span className={styleLabel.infolabel__boldfont}>
                     기본정보
                 </span>
-                <span class="text-danger">   *필수항목</span>
-                <div class="position-relative">
+                <span className="text-danger">   *필수항목</span>
+                <div className={`position-relative ${style.Settings__formBody}`}>
                     <Form.Item name="imgUpload d-inline p-2 position-absolute start-0">
-                        <Upload name="image" action="http://localhost:3000/image"
-                            listType="picture" showUploadList={false} onChange={onChangeImage}>
+                        <div className={style.Settings__CameraIcon__wrapper}>
 
-                            {/* 업로드 이미지가 있으면 이미지를 나타내고 업로드 이미지가 없으면
-                        회색배경에 업로드 아이콘이 나타나도록 ... */}
-                            {imageUrl ? <img src={imageUrl}
-                                alt="" width="200px" height="200px" /> :
-                                (<div id="upload-img-placeholder">
-                                    <img src="/camera.png" alt="" className={`cameraIcon ${style.Settings__CameraIcon__size}`} />
-                                </div>)}
-                        </Upload>
-                        <span className="text-primary">
-                            <span class="fw-semibold position-absolute fs-5 top-50 start-40 ">
-                                *이미지를 클릭할 경우 이미지 등록을 할 수 있습니다.
-                                <br>
-                                </br>
-                                <label class="fw-normal fs-6 styleLabel.smallfont">
-                                    -상품 이미지는 최대 5개까지 등록 가능합니다.<br>
-                                    </br>
-                                    -큰 이미지일 경우 이미지가 깨지는 경우가 발생할 수 있습니다.<br>
-                                    </br>
-                                    -대표이미지는 마지막으로 등록된 사진으로 보여집니다.
+                            <Upload name="image" action="http://localhost:3000/image"
+                                listType="picture" showUploadList={false} onChange={onChangeImage}>
+
+                                {/* 업로드 이미지가 있으면 이미지를 나타내고 업로드 이미지가 없으면
+                            회색배경에 업로드 아이콘이 나타나도록 ... */}
+                                {imageUrl ? <img src={imageUrl}
+                                    alt="" width="200px" height="200px" /> :
+                                    (<div id="upload-img-placeholder">
+                                        <div className={style.Settings__CameraIcon}>
+                                            <div className={style.Settings__CameraIcon__inner}>
+                                                <i className={`bi bi-camera-fill ${style.Settings__CameraIcon__icon}`}></i>
+                                                <div>이미지 등록</div>
+                                            </div>
+                                        </div>
+                                        {/* <img src="/camera.png" alt="" className={`cameraIcon ${style.Settings__CameraIcon__size}`} /> */}
+                                    </div>)}
+                            </Upload>
+                            <span className={`text-primary ${style.Settings__CameraIcon__label__wrapper}`}>
+                                <span className={style.Settings__CameraIcon__label__big}>
+                                    * 이미지 아이콘을 클릭하여 이미지 등록을 할 수 있습니다. <br />
+                                </span>
+                                <label className={style.Settings__CameraIcon__label__small}>
+                                        - 상품 이미지는 최대 3개까지 등록 가능합니다.<br>
+                                        </br>
+                                        - 큰 이미지일 경우 이미지가 깨지는 경우가 발생할 수 있습니다.<br>
+                                        </br>
+                                        - 대표이미지는 처음으로 등록된 사진으로 지정됩니다.
                                 </label>
+                                {/* <span className="fw-semibold position-absolute fs-5 top-50 start-40 ">
+                                    *이미지 아이콘을 클릭하여 이미지 등록을 할 수 있습니다.
+                                    <br>
+                                    </br>
+                                    <label className="fw-normal fs-6 styleLabel.smallfont">
+                                        - 상품 이미지는 최대 3개까지 등록 가능합니다.<br>
+                                        </br>
+                                        - 큰 이미지일 경우 이미지가 깨지는 경우가 발생할 수 있습니다.<br>
+                                        </br>
+                                        - 대표이미지는 처음으로 등록된 사진으로 지정됩니다.
+                                    </label>
+                                </span> */}
                             </span>
-                        </span>
+                        </div>
                     </Form.Item>
                 </div>
-                <form class="row g-3">
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button type="button" className={`btn btn-outline-dark ${styleBtn.Nav__SearchBtn} ${styleLabel.Btn}`}>
-                            등록하기
-                        </button>
-                    </div>
-                    <div class="row g-3">
+                <form className="row g-3">
+                    <div className="row g-3">
                         <div className={styleLabel.h1}></div>
-                        <div class="row mb-3">
-                            <label for="inputProName" class="col-sm-2 col-form-label">
-                                <label class="fs-3">제목</label>
-                                <label class="fs-3 text-danger">*</label>
+                        <div className="row">
+                            <label for="inputProName" className="col-sm-2 col-form-label">
+                                <label className="fs-3">제목</label>
+                                <label className="fs-3 text-danger">*</label>
                             </label>
-                            <div class="col-md-10">
-                                <input type="text" class="form-control" id="inputProName" required minlength="0" maxlength="30" placeholder="상품 제목을 입력하세요." />
-                                <div class="text-end"><span class="proname" onChange={lengthCheck}></span>/30</div>
+                            <div className="col-md-10">
+                                <input onChange={lengthCheck} type="text" className="form-control" id="inputProName" required minlength="0" maxlength="30" placeholder="상품 제목을 입력하세요." />
+                                <div className="text-end"><span className="proname"></span>
+                                {titleLength}/30</div>
                             </div>
                         </div>
                         <div className={styleLabel.h1}></div>
-                        <div class="col-md-6">
-                            <label for="inputAddress" class="form-label">카테고리<span class="text-danger">*</span></label>
-                            <select id="inputCategory" class="form-select">
+                        <div className="col-md-4">
+                            <label for="inputAddress" className="form-label">카테고리<span className="text-danger">*</span></label>
+                            <select id="inputCategory" className="form-select">
                                 <option selected>전체</option>
                                 <option>생활/주방/가구/건강</option>
                                 <option>가전/IT</option>
@@ -118,9 +137,9 @@ const Uploadpage = (props) => {
                             </select>
 
                         </div>
-                        <div class="col-md-6">
-                            <label for="inputAddress" class="form-label">지역<span class="text-danger">*</span></label>
-                            <select id="inputState" class="form-select">
+                        <div className="col-md-3">
+                            <label for="inputAddress" className="form-label">지역<span className="text-danger">*</span></label>
+                            <select id="inputState" className="form-select">
                                 <option selected>지역을 선택해주세요</option>
                                 <option>명동</option>
                                 <option>성수</option>
@@ -129,24 +148,34 @@ const Uploadpage = (props) => {
                                 <option>부산</option>
                             </select>
                         </div>
-                        <div className={styleLabel.h1}></div>
-                        <div class="col-md-6">
-                            <label for="inputCity" class="form-label">가격<span class="text-danger">*</span></label>
-                            <input type="text" class="form-control width=15px" id="inputCity" placeholder="숫자만 입력해주세요" />원
-                        </div>
-                        <div class="col-md-6">
-                            <label for="inputState" class="form-label">상태<span class="text-danger">*</span></label>
-                            <select id="inputState" class="form-select">
+                        <div className="col-md-2">
+                            <label for="inputState" className="form-label">상태<span className="text-danger">*</span></label>
+                            <select id="inputState" className="form-select">
                                 <option selected>판매중</option>
                                 <option>예약중</option>
                                 <option>판매완료</option>
                             </select>
                         </div>
                         <div className={styleLabel.h1}></div>
-                        <div class="col-12" className={styleLabel.textArea}>
-                            <label for="inputContent" class="form-label">상세설명<span class="text-danger">*</span></label>
-                            <textarea type="text" class="form-control" rows="5" id="inputContent" placeholder="여러 장의 상품 사진과 구입 연도, 브랜드, 사용감 하자 유무 등 구매자에게 필요한 정보를 꼭 포함해 주세요.(20자 이상)&#13;&#10;안전하고 건전한 거래 환경을 위해 신세계 I&C가 함께 합니다."></textarea>
+                        <div className={`col-md-3`}>
+                            <label for="inputCity" className="form-label">가격<span className="text-danger">*</span></label>
+                            <div className={styleLabel.priceInput__wrapper}>
+                                <input type="text" className={`form-control ${styleLabel.priceInput}`} id="inputCity" placeholder="숫자만 입력해주세요" />
+                                <span>원</span>
+                            </div>
                         </div>
+                        <div className={styleLabel.h1}></div>
+                        <div className="col-12">
+                            <label for="inputContent" className="form-label">상세설명<span className="text-danger">*</span></label>
+                            <textarea type="text"   min="20" max="500" onChange={(e) => setContentLength(e.target.value.length)}
+                             className={`form-control ${styleLabel.textArea}`} rows="5" id="inputContent" placeholder="여러 장의 상품 사진과 구입 연도, 브랜드, 사용감 하자 유무 등 구매자에게 필요한 정보를 꼭 포함해 주세요.(20자 이상)&#13;&#10;안전하고 건전한 거래 환경을 위해 신세계 I&C가 함께 합니다."></textarea>
+                            <div className={styleLabel.contentLength}>{contentLength}자</div>
+                        </div>
+                    </div>
+                    <div className="d-grid gap-2 d-md-flex justify-content-md-center">
+                        <button type="button" className={`btn btn-outline-dark ${styleBtn.Nav__SearchBtn} ${styleLabel.Btn}`}>
+                            등록하기
+                        </button>
                     </div>
                 </form>
             </form>
